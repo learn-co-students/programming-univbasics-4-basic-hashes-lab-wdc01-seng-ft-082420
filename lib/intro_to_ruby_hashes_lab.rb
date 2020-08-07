4e98c17e974e40d98323fc427b5eaf1bd12ca18b
lively-system-6174 <wkalada1@gmail.com>
2020-08-07 06:44:49 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,31 @@
 def new_hash
   # return an empty hash
+  new_hash = Hash.new
+  return new_hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = 
+  {
+    name: "kalada"
+  }
+  return my_hash
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = 
+  {
+    name: 'Grace Hopper'
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  new_hash = 
+  {
+    id: number
+  }
 end
